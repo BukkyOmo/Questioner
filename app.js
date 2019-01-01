53cbd48059eec063e536db5984bf0cbaf3326cea
Bukola Omosefunmi <odunayobukky1@gmail.com>
2019-01-01 18:39:12 +0100

    add babel-watch and implement build

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ app.get('/', (request, response) => {
 	response.send('Hello World');
 });
 
+app.get('/question', (request, response) => {
+	response.json({
+		data: 'i am blessed',
+	});
+});
+
 app.listen(8080, () => {
 	console.log('Questioner app listening on port 8080');
 });
