108a25aba3485f7418a2121a55c89f501eb740f8
Bukola Omosefunmi <odunayobukky1@gmail.com>
2019-01-06 11:52:21 +0100

    cover edge case for if endpoint is not in database

--- test/test.js
+++ test/test.js
@@ -1,5 +1,4 @@
 import app from '../app';
-import meetup from '../v1/models/database';
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
@@ -13,9 +12,24 @@ describe('Get a meetup', () => {
 		chai.request(app)
 			.get('/api/v1/meetups/1')
 			.end((err, res) => {
+				if (err) {
+					done(err);
+				}
 				expect(res).to.have.status(200);
 				expect(res.body.status).to.be.equal(200);
 				expect(res.body.message).to.be.equal(true);
+				done();
+			});
+	});
+
+	it('it should not get a specific meetup with invalid id', (done) => {
+		chai.request(app)
+			.get('/api/v1/meetups/3')
+			.end((err, res) => {
+				if (err) {
+					done(err);
+				}
+				expect(res.body.status).equal(404);
 				done();
 			});
 	});
@@ -24,6 +38,9 @@ describe('Get a meetup', () => {
 		chai.request(app)
 			.get('/api/v1/meetups')
 			.end((err, res) => {
+				if (err) {
+					done(err);
+				}
 				expect(res).to.have.status(200);
 				expect(res.body.status).to.be.equal(200);
 				expect(res.body.message).to.be.equal(true);
