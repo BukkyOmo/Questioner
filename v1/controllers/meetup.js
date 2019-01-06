ef86b19ad428c7930964f043dcd2256f740b97c7
Bukola Omosefunmi <odunayobukky1@gmail.com>
2019-01-06 09:03:59 +0100

    fix test

--- v1/controllers/meetup.js
+++ v1/controllers/meetup.js
@@ -1,26 +1,23 @@
 import meetup from '../models/database';
 
 const getMeetup = (req, res) => {
-	// console.log(Number(req.params.meetupId));
-	// const { meetupId } = request.params;
 	const oneMeetup = meetup.find(onemeetup => onemeetup.id === Number(req.params.meetupId));
 	if (!oneMeetup) {
-		res.json({
+		return res.json({
 			status: 404,
 			message: false,
 			error: 'This meetup record cannot be found'
 		});
-	} else {
-		res.json({
-			status: 200,
-			message: true,
-			data: [{
-				id: oneMeetup.id,
-				topic: oneMeetup.topic,
-				location: oneMeetup.location,
-				happeningOn: oneMeetup.happeningOn
-			}]
-		});
 	}
+	return res.json({
+		status: 200,
+		message: true,
+		data: [{
+			id: oneMeetup.id,
+			topic: oneMeetup.topic,
+			location: oneMeetup.location,
+			happeningOn: oneMeetup.happeningOn
+		}]
+	});
 };
 export default getMeetup;
