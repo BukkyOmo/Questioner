e057e9545c436e6b93683b346c6c35d0ee950471
Bukola Omosefunmi <odunayobukky1@gmail.com>
2019-01-06 11:03:04 +0100

    implement get all meetup endpoint

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
 import app from '../app';
+import meetup from '../v1/models/database';
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
@@ -11,6 +12,17 @@ describe('Get a meetup', () => {
 	it('it should get a specific meetup', (done) => {
 		chai.request(app)
 			.get('/api/v1/meetups/1')
+			.end((err, res) => {
+				expect(res).to.have.status(200);
+				expect(res.body.status).to.be.equal(200);
+				expect(res.body.message).to.be.equal(true);
+				done();
+			});
+	});
+
+	it('it should get all meetups', (done) => {
+		chai.request(app)
+			.get('/api/v1/meetups')
 			.end((err, res) => {
 				expect(res).to.have.status(200);
 				expect(res.body.status).to.be.equal(200);
