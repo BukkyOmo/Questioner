78e9ef19f24ef18232a5a493d5da4bbe35749aeb
Bukola Omosefunmi <odunayobukky1@gmail.com>
2019-01-07 14:30:02 +0100

    implement endpoint for question upvote

--- app/v1/controllers/question.js
+++ app/v1/controllers/question.js
@@ -35,7 +35,30 @@ const questionController = {
 				data: 'meetup cannot be created'
 			}
 		);
-	}
+	},
+
+	upvoteQuestion(request, response) {
+		const findQuestion = question
+			.find(onequestion => onequestion.id === Number(request.params.id));
+		if (!findQuestion) {
+			return response.json({
+				status: 404,
+				message: false,
+				data: 'The question you tried to upvote does not exist'
+			});
+		}
+		findQuestion.votes += 1;
+		return response.json({
+			status: 200,
+			message: true,
+			data: [{
+				meetup: request.params.meetupId,
+				title: findQuestion.title,
+				content: findQuestion.content,
+				votes: findQuestion.votes
+			}]
+		});
+	},
 };
 
 export default questionController;
