c55961ee82990f125f14cb7ac83cfe70a188188f
Bukola Omosefunmi <odunayobukky1@gmail.com>
2019-01-09 11:45:10 +0100

    refactored my routes file

--- app/v1/routes/routes.js
+++ app/v1/routes/routes.js
@@ -2,7 +2,6 @@ import express from 'express';
 import meetupController from '../controllers/meetup';
 import questionController from '../controllers/question';
 import rsvpController from '../controllers/rsvp';
-import userController from '../controllers/user';
 
 const router = express.Router();
 
@@ -17,8 +16,7 @@ router.route('/meetups/:meetupsId/questions')
 	.post(questionController.createQuestion);
 
 router.route('/meetups/:meetupId/questions/:id')
-	.patch(questionController.upvoteQuestion)
-	.patch(questionController.downvoteQuestion);
+	.patch(questionController.upvoteQuestion);
 
 router.route('/meetups/:meetupId/rsvp')
 	.post(rsvpController.createRsvp);
