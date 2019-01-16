c3e8eac787f6c2285de92faf788cee15d9b5e89a
Bukola Omosefunmi <odunayobukky1@gmail.com>
2019-01-16 15:52:47 +0100

    [chore #163180378] Implement a check if user already exist in db

--- app/v1/dbControllers/user.js
+++ app/v1/dbControllers/user.js
@@ -5,26 +5,36 @@ class UserController {
 		const {
 			firstname, lastname, username, phoneNumber, email, password
 		} = request.body;
-		const selectQuery = {
-			text: 'INSERT INTO users (firstname, lastname, username, phoneNumber, email, password, isadmin) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *',
-			values: [firstname, lastname, username, phoneNumber, email, password, false]
-		};
-		pool.query(selectQuery).then((users) => {
-			console.log(users);
-			if (users.rows) {
-				return response.status(201).json({
-					status: 201,
-					message: true,
-					data: [users.rows[0]]
-				});
-			}
-		}).catch(error => (
-			response.status(400).json({
-				status: 400,
-				message: false,
-				error: ({ message: 'username or email already exists' })
-			})
-		));
+		pool.query({ text: 'SELECT email FROM users WHERE email = $1', values: [email] })
+			.then((result) => {
+				console.log(result);
+				if (result.rows.length > 0) {
+					return response.status(409).json({
+						status: 409,
+						message: false,
+						error: ({ message: 'user already exists' })
+					});
+				}
+				const selectQuery = {
+					text: 'INSERT INTO users (firstname, lastname, username, phoneNumber, email, password, isadmin) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *',
+					values: [firstname, lastname, username, phoneNumber, email, password, false]
+				};
+				pool.query(selectQuery).then((users) => {
+					if (users.rows) {
+						return response.status(201).json({
+							status: 201,
+							message: true,
+							data: [users.rows[0]]
+						});
+					}
+				}).catch(error => (
+					response.status(500).json({
+						status: 500,
+						message: false,
+						error: ({ message: 'Internal server error' })
+					})
+				));
+			});
 	}
 }
 export default UserController;
