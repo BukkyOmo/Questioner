c4727ac03fe62782c033a2bf69a6d627598738e1
Bukola Omosefunmi <odunayobukky1@gmail.com>
2019-01-18 10:00:31 +0100

    [feature 163315017]implement endpoint for get all meetups

--- app/v1/routes/routes.js
+++ app/v1/routes/routes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import meetupValidator from '../middleware/meetupValidation';
+import signupValidator from '../middleware/signupValidation';
 import questionVlidator from '../middleware/questionValidation';
 import UserController from '../dbControllers/user';
 import MeetupController from '../dbControllers/meetup';
@@ -7,10 +8,10 @@ import MeetupController from '../dbControllers/meetup';
 const router = express.Router();
 
 router.route('/auth/signup')
-	.post(UserController.signup);
+	.post(signupValidator.signupValidator, UserController.signup);
 
 router.route('/auth/signin')
-	.post(UserController.signin);
+	.post(signupValidator.signupValidator, UserController.signin);
 
 router.route('/meetups')
 	.get(MeetupController.getAllMeetups)
