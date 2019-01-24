9dbc4955e090f8ab2550a4381d687fb3fd69f29b
Bukola Omosefunmi <odunayobukky1@gmail.com>
2019-01-24 09:23:25 +0100

    [bug]fix test failing on travis

--- app/test/usertest.js
+++ app/test/usertest.js
@@ -1,261 +1,262 @@
-// import app from '../app';
+import app from '../app';
 
-// const chai = require('chai');
-// const chaiHttp = require('chai-http');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
 
-// chai.use(chaiHttp);
-
-
-// const { expect } = chai;
-
-// describe('TEST ALL USER ENDPOINTS', () => {
-// 	it.only('it should create a user that is not already in database', (done) => {
-// 		const newUser = {
-// 			firstname: 'bukola',
-// 			lastname: 'Odunayo',
-// 			password: 'Buks',
-// 			email: 'odunbukola@gmail.com',
-// 			phoneNumber: '09039136484',
-// 			username: 'bukkade12'
-// 		};
-// 		chai.request(app)
-// 			.post('/api/v1/auth/signup')
-// 			.send(newUser)
-// 			.end((err, res) => {
-// 				console.log(err);
-// 				expect(res).to.have.status(201);
-// 				expect(res.body.status).to.be.equal(201);
-// 				expect(res.body.message).to.be.equal('Your registration was successful');
-// 				done();
-// 			});
-// 	});
-
-// 	it.only('it should not create a user whose email is not unique', (done) => {
-// 		const myUser = {
-// 			firstname: 'bukola',
-// 			lastname: 'Odunayo',
-// 			password: 'Buks',
-// 			email: 'odunbukola@gmail.com',
-// 			phoneNumber: '09039136484',
-// 			username: 'bukkadeye'
-// 		};
-// 		chai.request(app)
-// 			.post('/api/v1/auth/signup')
-// 			.send(myUser)
-// 			.end((err, res) => {
-// 				expect(res).to.have.status(409);
-// 				expect(res.body.status).to.be.equal(409);
-// 				expect(res.body.message).to.be.equal('User already exists');
-// 				done();
-// 			});
-// 	});
-
-// 	it.only('it should not create a user whose username is not unique', (done) => {
-// 		const newUser = {
-// 			firstname: 'bukola',
-// 			lastname: 'Odunayo',
-// 			password: 'Buks',
-// 			email: 'odunbukola12@gmail.com',
-// 			phoneNumber: '09039136484',
-// 			username: 'bukkade12'
-// 		};
-// 		chai.request(app)
-// 			.post('/api/v1/auth/signup')
-// 			.send(newUser)
-// 			.end((err, res) => {
-// 				expect(res).to.have.status(409);
-// 				expect(res.body.status).to.be.equal(409);
-// 				expect(res.body.message).to.be.equal('User already exists');
-// 				done();
-// 			});
-// 	});
-
-// 	it.only('it should not log in a user who is not in database', (done) => {
-// 		const newUser = {
-// 			email: 'odmreferral@gmail.com',
-// 			password: '34567'
-// 		};
-// 		chai.request(app)
-// 			.post('/api/v1/auth/signin')
-// 			.send(newUser)
-// 			.end((err, res) => {
-// 				expect(res).to.have.status(404);
-// 				expect(res.body.status).to.be.equal(404);
-// 				expect(res.body.message).to.be.equal(false);
-// 				done();
-// 			});
-// 	});
-
-// 	it.only('it should not log in a user whose email is incorrect', (done) => {
-// 		const newUser = {
-// 			email: 'odunbabey11@gmail.com',
-// 			password: 'flexy'
-// 		};
-// 		chai.request(app)
-// 			.post('/api/v1/auth/signin')
-// 			.send(newUser)
-// 			.end((err, res) => {
-// 				expect(res).to.have.status(404);
-// 				expect(res.body.status).to.be.equal(404);
-// 				expect(res.body.message).to.be.equal(false);
-// 				done();
-// 			});
-// 	});
-
-// 	it.only('it should not log in a user whose password is incorrect', (done) => {
-// 		const newUser = {
-// 			email: 'odunbukola@gmail.com',
-// 			password: 'bukkade12'
-// 		};
-// 		chai.request(app)
-// 			.post('/api/v1/auth/signin')
-// 			.send(newUser)
-// 			.end((err, res) => {
-// 				expect(res).to.have.status(409);
-// 				expect(res.body.status).to.be.equal(409);
-// 				expect(res.body.message).to.be.equal(false);
-// 				done();
-// 			});
-// 	});
-// });
-
-// describe('TEST ALL MEETUP ENDPOINTS', () => {
-// 	it.only('it should create a meetup that is not already in database', (done) => {
-// 		const newMeetup = {
-// 			topic: 'God saves',
-// 			location: 'Anambra',
-// 			happeningOn: '2019-04-02',
-// 			tags: 'event'
-// 		};
-// 		chai.request(app)
-// 			.post('/api/v1/meetups')
-// 			.send(newMeetup)
-// 			.end((err, res) => {
-// 				console.log(err);
-// 				expect(res).to.have.status(201);
-// 				expect(res.body.status).to.be.equal(201);
-// 				expect(res.body.message).to.be.equal('Your registration was successful');
-// 				done();
-// 			});
-// 	});
-
-// 	it.only('it should get all meetups', (done) => {
-// 		chai.request(app)
-// 			.get('/api/v1/meetups')
-// 			.end((err, res) => {
-// 				expect(res).to.have.status(200);
-// 				expect(res.body.success).to.be.equal(true);
-// 				expect(res.body.message).to.be.equal('Successfully Retrived all meetups');
-// 				done();
-// 			});
-// 	});
-// });
+chai.use(chaiHttp);
 
 
-// describe('TEST ALL MIDDLEWARES', () => {
-// 	it.only('it should throw an error when the topic is not a string', (done) => {
-// 		const newMeetup = {
-// 			createdOn: '3-12-2018',
-// 			location: 'Abuja',
-// 			topic: 12345,
-// 			happeningOn: '15-02-2018',
-// 			tags: ['flowers', 'love']
-// 		};
-// 		chai.request(app)
-// 			.post('/api/v1/meetups')
-// 			.send(newMeetup)
-// 			.end((err, res) => {
-// 				expect(res).to.have.status(400);
-// 				expect(res.body.error).to.be.equal(true);
-// 				done();
-// 			});
-// 	});
+const { expect } = chai;
 
-// 	it.only('it should throw an error when the topic is empty', (done) => {
-// 		const newMeetup = {
-// 			createdOn: '3-12-2018',
-// 			location: 'Abuja',
-// 			topic: '',
-// 			happeningOn: '15-02-2018',
-// 			tags: ['flowers', 'love']
-// 		};
-// 		chai.request(app)
-// 			.post('/api/v1/meetups')
-// 			.send(newMeetup)
-// 			.end((err, res) => {
-// 				expect(res).to.have.status(400);
-// 				expect(res.body.error).to.be.equal(true);
-// 				done();
-// 			});
-// 	});
+describe('TEST ALL USER ENDPOINTS', () => {
+	it.only('it should create a user that is not already in database', (done) => {
+		const newUser = {
+			firstname: 'bukola',
+			lastname: 'Odunayo',
+			password: 'Buks',
+			email: 'odunbukola@gmail.com',
+			phoneNumber: '09039136484',
+			username: 'bukkade12'
+		};
+		chai.request(app)
+			.post('/api/v1/auth/signup')
+			.send(newUser)
+			.end((err, res) => {
+				console.log(err);
+				console.log(res);
+				expect(res).to.have.status(201);
+				expect(res.body.status).to.be.equal(201);
+				expect(res.body.message).to.be.equal('Your registration was successful');
+				done();
+			});
+	});
 
-// 	it.only('it should throw an error when the topic is not a string', (done) => {
-// 		const newMeetup = {
-// 			createdOn: '3-12-2018',
-// 			location: 'Abuja',
-// 			topic: 125678,
-// 			happeningOn: '15-02-2018',
-// 			tags: ['flowers', 'love']
-// 		};
-// 		chai.request(app)
-// 			.post('/api/v1/meetups')
-// 			.send(newMeetup)
-// 			.end((err, res) => {
-// 				expect(res).to.have.status(400);
-// 				expect(res.body.error).to.be.equal(true);
-// 				done();
-// 			});
-// 	});
+	it.only('it should not create a user whose email is not unique', (done) => {
+		const myUser = {
+			firstname: 'bukola',
+			lastname: 'Odunayo',
+			password: 'Buks',
+			email: 'odunbukola@gmail.com',
+			phoneNumber: '09039136484',
+			username: 'bukkadeye'
+		};
+		chai.request(app)
+			.post('/api/v1/auth/signup')
+			.send(myUser)
+			.end((err, res) => {
+				expect(res).to.have.status(409);
+				expect(res.body.status).to.be.equal(409);
+				expect(res.body.message).to.be.equal('User already exists');
+				done();
+			});
+	});
 
-// 	it.only('it should throw an error when the location is empty', (done) => {
-// 		const newMeetup = {
-// 			createdOn: '3-12-2018',
-// 			topic: 'The influx of gayism in Nigeria',
-// 			happeningOn: '15-02-2018',
-// 			tags: ['flowers', 'love']
-// 		};
-// 		chai.request(app)
-// 			.post('/api/v1/meetups')
-// 			.send(newMeetup)
-// 			.end((err, res) => {
-// 				expect(res).to.have.status(400);
-// 				expect(res.body.error).to.be.equal(true);
-// 				done();
-// 			});
-// 	});
+	it.only('it should not create a user whose username is not unique', (done) => {
+		const newUser = {
+			firstname: 'bukola',
+			lastname: 'Odunayo',
+			password: 'Buks',
+			email: 'odunbukola12@gmail.com',
+			phoneNumber: '09039136484',
+			username: 'bukkade12'
+		};
+		chai.request(app)
+			.post('/api/v1/auth/signup')
+			.send(newUser)
+			.end((err, res) => {
+				expect(res).to.have.status(409);
+				expect(res.body.status).to.be.equal(409);
+				expect(res.body.message).to.be.equal('User already exists');
+				done();
+			});
+	});
 
-// 	it.only('it should throw an error when the location is not a string', (done) => {
-// 		const newMeetup = {
-// 			createdOn: '3-12-2018',
-// 			topic: 'The influx of gayism in Nigeria',
-// 			location: 56879,
-// 			happeningOn: '15-02-2018',
-// 			tags: ['flowers', 'love']
-// 		};
-// 		chai.request(app)
-// 			.post('/api/v1/meetups')
-// 			.send(newMeetup)
-// 			.end((err, res) => {
-// 				expect(res).to.have.status(400);
-// 				expect(res.body.error).to.be.equal(true);
-// 				done();
-// 			});
-// 	});
+	it.only('it should not log in a user who is not in database', (done) => {
+		const newUser = {
+			email: 'odmreferral@gmail.com',
+			password: '34567'
+		};
+		chai.request(app)
+			.post('/api/v1/auth/signin')
+			.send(newUser)
+			.end((err, res) => {
+				expect(res).to.have.status(404);
+				expect(res.body.status).to.be.equal(404);
+				expect(res.body.message).to.be.equal(false);
+				done();
+			});
+	});
 
-// 	it.only('it should throw an error when the happening date is empty', (done) => {
-// 		const newMeetup = {
-// 			createdOn: '3-12-2018',
-// 			topic: 'The influx of gayism in Nigeria',
-// 			location: 'Lokoja',
-// 			tags: ['flowers', 'love']
-// 		};
-// 		chai.request(app)
-// 			.post('/api/v1/meetups')
-// 			.send(newMeetup)
-// 			.end((err, res) => {
-// 				expect(res).to.have.status(400);
-// 				expect(res.body.error).to.be.equal(true);
-// 				done();
-// 			});
-// 	});
-// });
+	it.only('it should not log in a user whose email is incorrect', (done) => {
+		const newUser = {
+			email: 'odunbabey11@gmail.com',
+			password: 'flexy'
+		};
+		chai.request(app)
+			.post('/api/v1/auth/signin')
+			.send(newUser)
+			.end((err, res) => {
+				expect(res).to.have.status(404);
+				expect(res.body.status).to.be.equal(404);
+				expect(res.body.message).to.be.equal(false);
+				done();
+			});
+	});
+
+	it.only('it should not log in a user whose password is incorrect', (done) => {
+		const newUser = {
+			email: 'odunbukola@gmail.com',
+			password: 'bukkade12'
+		};
+		chai.request(app)
+			.post('/api/v1/auth/signin')
+			.send(newUser)
+			.end((err, res) => {
+				expect(res).to.have.status(409);
+				expect(res.body.status).to.be.equal(409);
+				expect(res.body.message).to.be.equal(false);
+				done();
+			});
+	});
+});
+
+describe('TEST ALL MEETUP ENDPOINTS', () => {
+	it.only('it should create a meetup that is not already in database', (done) => {
+		const newMeetup = {
+			topic: 'God saves',
+			location: 'Anambra',
+			happeningOn: '2019-04-02',
+			tags: 'event'
+		};
+		chai.request(app)
+			.post('/api/v1/meetups')
+			.send(newMeetup)
+			.end((err, res) => {
+				console.log(err);
+				expect(res).to.have.status(201);
+				expect(res.body.status).to.be.equal(201);
+				expect(res.body.message).to.be.equal('Your registration was successful');
+				done();
+			});
+	});
+
+	it.only('it should get all meetups', (done) => {
+		chai.request(app)
+			.get('/api/v1/meetups')
+			.end((err, res) => {
+				expect(res).to.have.status(200);
+				expect(res.body.success).to.be.equal(true);
+				expect(res.body.message).to.be.equal('Successfully Retrived all meetups');
+				done();
+			});
+	});
+});
+
+
+describe('TEST ALL MIDDLEWARES', () => {
+	it.only('it should throw an error when the topic is not a string', (done) => {
+		const newMeetup = {
+			createdOn: '3-12-2018',
+			location: 'Abuja',
+			topic: 12345,
+			happeningOn: '15-02-2018',
+			tags: ['flowers', 'love']
+		};
+		chai.request(app)
+			.post('/api/v1/meetups')
+			.send(newMeetup)
+			.end((err, res) => {
+				expect(res).to.have.status(400);
+				expect(res.body.error).to.be.equal(true);
+				done();
+			});
+	});
+
+	it.only('it should throw an error when the topic is empty', (done) => {
+		const newMeetup = {
+			createdOn: '3-12-2018',
+			location: 'Abuja',
+			topic: '',
+			happeningOn: '15-02-2018',
+			tags: ['flowers', 'love']
+		};
+		chai.request(app)
+			.post('/api/v1/meetups')
+			.send(newMeetup)
+			.end((err, res) => {
+				expect(res).to.have.status(400);
+				expect(res.body.error).to.be.equal(true);
+				done();
+			});
+	});
+
+	it.only('it should throw an error when the topic is not a string', (done) => {
+		const newMeetup = {
+			createdOn: '3-12-2018',
+			location: 'Abuja',
+			topic: 125678,
+			happeningOn: '15-02-2018',
+			tags: ['flowers', 'love']
+		};
+		chai.request(app)
+			.post('/api/v1/meetups')
+			.send(newMeetup)
+			.end((err, res) => {
+				expect(res).to.have.status(400);
+				expect(res.body.error).to.be.equal(true);
+				done();
+			});
+	});
+
+	it.only('it should throw an error when the location is empty', (done) => {
+		const newMeetup = {
+			createdOn: '3-12-2018',
+			topic: 'The influx of gayism in Nigeria',
+			happeningOn: '15-02-2018',
+			tags: ['flowers', 'love']
+		};
+		chai.request(app)
+			.post('/api/v1/meetups')
+			.send(newMeetup)
+			.end((err, res) => {
+				expect(res).to.have.status(400);
+				expect(res.body.error).to.be.equal(true);
+				done();
+			});
+	});
+
+	it.only('it should throw an error when the location is not a string', (done) => {
+		const newMeetup = {
+			createdOn: '3-12-2018',
+			topic: 'The influx of gayism in Nigeria',
+			location: 56879,
+			happeningOn: '15-02-2018',
+			tags: ['flowers', 'love']
+		};
+		chai.request(app)
+			.post('/api/v1/meetups')
+			.send(newMeetup)
+			.end((err, res) => {
+				expect(res).to.have.status(400);
+				expect(res.body.error).to.be.equal(true);
+				done();
+			});
+	});
+
+	it.only('it should throw an error when the happening date is empty', (done) => {
+		const newMeetup = {
+			createdOn: '3-12-2018',
+			topic: 'The influx of gayism in Nigeria',
+			location: 'Lokoja',
+			tags: ['flowers', 'love']
+		};
+		chai.request(app)
+			.post('/api/v1/meetups')
+			.send(newMeetup)
+			.end((err, res) => {
+				expect(res).to.have.status(400);
+				expect(res.body.error).to.be.equal(true);
+				done();
+			});
+	});
+});
--- app/v1/dbControllers/user.js
+++ app/v1/dbControllers/user.js
@@ -15,6 +15,7 @@ class UserController {
 		} = request.body;
 		const hash = passwordhash.generate(password);
 		pool.query({ text: 'SELECT * FROM users WHERE email = $1 OR username = $2', values: [email, username] })
+			// eslint-disable-next-line consistent-return
 			.then((result) => {
 				if (result.rows.length > 0) {
 					return response.status(409).json({
@@ -27,6 +28,7 @@ class UserController {
 					text: 'INSERT INTO users (firstname, lastname, username, phoneNumber, email, password, isadmin) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *',
 					values: [firstname, lastname, username, phoneNumber, email, hash, false]
 				};
+				// eslint-disable-next-line consistent-return
 				pool.query(selectQuery).then((users) => {
 					if (users.rows) {
 						const token = jwt.sign({ id: users.rows[0].user_id, isAdmin: users.rows[0].isadmin }, secret, { expiresIn: '1h' });
