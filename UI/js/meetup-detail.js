5e0592e2506e0939a6200b8d47eb8ccc65910813
phunmbi <phunmbi@gmail.com>
2019-02-07 13:30:22 +0100

    [feature #163741225]Implement fetch to get a specific meetup endpoint

--- UI/js/meetup-detail.js
+++ UI/js/meetup-detail.js
@@ -1,5 +1,11 @@
-const url = "https://bukkyomo-questioner.herokuapp.com/api/v1/meetups/:id";
+const id = sessionStorage.getItem("meetupId");
 const token = localStorage.getItem("token");
+const meetupContainer = document.getElementsByClassName("meetup-flex")[0];
+if (!id) {
+    window.location.href = 'meetups.html';
+}
+
+const url = `https://bukkyomo-questioner.herokuapp.com/api/v1/meetups/${id}`;
 
 const fetchData = {
     method: 'GET',
@@ -11,6 +17,34 @@ fetch(url, fetchData)
     .then((resp) => {
         const { data } = resp;
         if (data) {
-         console.log(data);
+           const meetupCard = `<div>
+
+                <img src="${
+                  data[0].images
+                }" alt="specific-meetup" class="meetup-img">
+
+				</div>
+
+                <div class="meetup-details">
+
+                    <h3>${data[0].topic}</h3>
+
+                    <h4>${data[0].location}</h4>
+
+                    <h5>${new Date(data[0].happeningon).toDateString()}</h5>
+
+                    <h6>Tags: Workshop, React, Node</h6>
+
+                    <div>
+
+                        <button class="btn-bg"><i class="fas fa-calendar-check fa-2x"></i></button>
+
+                        <span>Rsvp this meetup</span>
+
+                    </div>
+
+                </div>`;
+            meetupContainer.insertAdjacentHTML('afterbegin', meetupCard)
         }
     })
+
