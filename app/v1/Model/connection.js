23d893c26ff5dca386cc1978a4ea8d974575a7c5
BukkyOmo <odunayobukky1@gmail.com>
2020-04-12 21:52:59 +0100

    set production config env

--- app/v1/Model/connection.js
+++ app/v1/Model/connection.js
@@ -3,10 +3,10 @@ import { Pool } from 'pg';
 const config = {
 	development: process.env.DATABASE_URL,
 	test: process.env.TEST_DATABASE_URL,
-	production: process.env.PRODUCTION_URL,
+	production: process.env.PRODUCTION_DATABASE_URL,
 };
 const env = process.env.NODE_ENV;
-console.log(config[env])
+
 const pool = new Pool({
 	connectionString: config[env]
 });
